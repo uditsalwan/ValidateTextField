f074c1ae728a8b9707b356e0f73e509197e979b0
Udit <udit.salwan@gmail.com>
2019-06-28 12:42:52 +0530

    Initial Project setup

---
+++ ValidateTextField/ValidateTextField.h
@@ -0,0 +1,19 @@
+//
+//  ValidateTextField.h
+//  ValidateTextField
+//
+//  Created by Udit on 28/06/19.
+//  Copyright © 2019 uDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValidateTextField.
+FOUNDATION_EXPORT double ValidateTextFieldVersionNumber;
+
+//! Project version string for ValidateTextField.
+FOUNDATION_EXPORT const unsigned char ValidateTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidateTextField/PublicHeader.h>
+
+
---
+++ ValidateTextFieldExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValidateTextFieldExample
+//
+//  Created by Udit on 28/06/19.
+//  Copyright © 2019 uDemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
